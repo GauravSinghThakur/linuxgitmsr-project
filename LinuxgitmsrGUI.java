bdd1f67420c825351767189a1b1a212f57e7a223
GauravSinghThakur <gst.coldplayer@gmail.com>
2016-04-09 23:44:12 -0700

    adding splitpane to GUI

--- LinuxgitmsrGUI.java
+++ LinuxgitmsrGUI.java
@@ -7,6 +7,7 @@ import java.awt.event.MouseListener;
 import java.sql.Connection;
 import java.util.ArrayList;
 
+import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -19,6 +20,7 @@ import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreePath;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.JSplitPane;
+import java.awt.Rectangle;
 
 public class LinuxgitmsrGUI {
 
@@ -64,39 +66,63 @@ public class LinuxgitmsrGUI {
 		frame.setBounds(100, 100, 1019, 813);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		// frame -> container -> ScrollPane -> Tree
-		Container content = frame.getContentPane();
-		JTree tree = new JTree();
-		JScrollPane scrolltree = new JScrollPane(tree);
-		content.add(scrolltree, BorderLayout.CENTER);
-		//frame.setVisible(true);		
+		// frame -> container -> panel -> ScrollPane -> Tree
 		
-		//Step 1: checking the postgres connection with DB linuxgitmsr
-		postgres = new DbConnect();
-		//Step 2: Connection to the DB
-		con = postgres.connectToPg();
-		//Step 3: Pass the connection object to CommitTree
-		ct = new CommitTree();
-		ct.setConnection(con);
-		//Step 4: get mcidlinus in an ArrayList
-		mcidlinus = ct.getMcidlinus();
-		//Step 5: Close Connection to DB
-		postgres.closeConnection(con);
-				
-		// add mcidlinus to tree node
-		tree.setModel(new DefaultTreeModel(
+		 JSplitPane splitPane = new JSplitPane();
+	     JPanel leftPanel = new JPanel();
+	     JTree tree = new JTree();
+	     JPanel rightPanel = new JPanel();
+	     JEditorPane editor = new JEditorPane();
+	     JScrollPane scroller = new JScrollPane(editor);
+	     JScrollPane treeScroller = new JScrollPane(tree);
+
+	     JPanel outerBox = new JPanel();
+
+	     leftPanel.setLayout(new BorderLayout() );
+	     leftPanel.add(treeScroller, BorderLayout.CENTER);
+	    
+	     rightPanel.setLayout(new BorderLayout() );
+	     rightPanel.add(scroller, BorderLayout.CENTER);
+
+
+	     splitPane.setLeftComponent(leftPanel);
+	     splitPane.setRightComponent(rightPanel);
+
+
+	       outerBox.setLayout (new BorderLayout ( ) );
+	       outerBox.add ("Center", splitPane);
+
+	     frame.getContentPane().add(outerBox);
+		
+			//Step 1: checking the postgres connection with DB linuxgitmsr
+				postgres = new DbConnect();
+				//Step 2: Connection to the DB
+				con = postgres.connectToPg();
+				//Step 3: Pass the connection object to CommitTree
+				ct = new CommitTree();
+				ct.setConnection(con);
+				//Step 4: get mcidlinus in an ArrayList
+				mcidlinus = ct.getMcidlinus();
+				//Step 5: Close Connection to DB
+				postgres.closeConnection(con);
+		
+		
+			// add mcidlinus to tree node
+		
+			tree.setModel(new DefaultTreeModel(
 			new DefaultMutableTreeNode("Hierarchy") {
-				{
-					DefaultMutableTreeNode node_1;
-					node_1 = new DefaultMutableTreeNode("mcidlinus");
-					for (int i=0; i<mcidlinus.size(); i++){
-						node_1.add(new DefaultMutableTreeNode(mcidlinus.get(i).getMcidlinus()));
-						add(node_1);
-					}
-				}
+		{
+			DefaultMutableTreeNode node_1;
+			node_1 = new DefaultMutableTreeNode("mcidlinus");
+			for (int i=0; i<mcidlinus.size(); i++){
+				node_1.add(new DefaultMutableTreeNode(mcidlinus.get(i).getMcidlinus()));
+				add(node_1);
+			}
+		}
 			}
 		));
-		
+								
+		// mouse onclick event		
 		MouseListener ml = new MouseAdapter() {
 		    public void mousePressed(MouseEvent e) {
 		        int selRow = tree.getRowForLocation(e.getX(), e.getY());
